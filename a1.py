b7e2449ba0e21d9add099baadf73daef99e14c22
Trackdown12 <12707vinaykumar.co@gmail.com>
2024-11-25 07:49:07 +0530

    ..

--- a1.py
+++ a1.py
@@ -1,7 +1,21 @@
-a=5
-for i in range(0,a):
-    print(" "*(a-i-1),end="")
-    for j in range(0,i+1):
-        print(i-j+1,end="")
-    print()
-    a=5
+import time
+
+start_time = time.time()
+
+# Your code here
+# Example:
+for i in range(1000000):
+    pass
+
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Execution time: {execution_time} seconds")
+
+#a=5
+#for i in range(0,a):
+#    print(" "*(a-i-1),end="")
+#    for j in range(0,i+1):
+#        print(i-j+1,end="")
+#    print()
+#    a=5
+    
